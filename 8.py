5de594a6fcd8b1783b6fbfb65761fdc2689c8f6e
Varun V Kudli <kudlivarun@gmail.com>
2025-06-10 18:40:51 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,24 +1,17 @@
-
 pip install langchain cohere langchain-community google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client
-
-
+import os
+from langchain.llms import Cohere
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 from google.colab import drive
+
 drive.mount('/content/drive')
-
-
 file_path = '/content/drive/MyDrive/car.txt'
 with open(file_path, 'r') as file:
     document_text = file.read()
 
-
-import os
-from langchain.llms import Cohere
-
 os.environ["COHERE_API_KEY"] = "o6DdGd0awe4vhcOEBS4r3RJOt0PdNB4iC60lIE40"
 llm = Cohere(cohere_api_key=os.getenv("COHERE_API_KEY"))
-
-
-from langchain.prompts import PromptTemplate
 
 prompt = PromptTemplate(
     input_variables=["document"],
@@ -32,13 +25,6 @@ Summary:
 """
 )
 
-
-from langchain.chains import LLMChain
-
 chain = LLMChain(llm=llm, prompt=prompt)
-
-
 response = chain.run(document=document_text)
-
-
 print(response)
